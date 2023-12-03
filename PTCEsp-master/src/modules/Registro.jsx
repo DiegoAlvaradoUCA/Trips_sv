d8b552ca087316b7d9fabfad1727f0e1c2368e8b
DiegoAlvaradoUCA <00127522@uca.edu.sv>
2023-12-03 00:28:43 -0600

    images

--- PTCEsp-master/src/modules/Registro.jsx
+++ PTCEsp-master/src/modules/Registro.jsx
@@ -19,7 +19,7 @@ function Registro() {
           src="https://cdn-icons-png.flaticon.com/512/3534/3534139.png"
           alt="image 1"
           className="h-full w-full object-cover"
-          style={{ width: '200px' }}
+          style={{ width: '50px' }}
         />
       </div>
       <div className="w-2/5 pr-8 m-auto">
